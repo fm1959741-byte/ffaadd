1d6b2208f9f698d6e67a7b53654178afb0a0ccf0
fatoo <fm1959741@gmail.com>
2026-01-29 17:50:44 +0300

    Fixed requirements and added name

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Restaurant System
+Project Documentation:
+
+_ **Project Name: ** ffaadd
+_ **Data Structure used:** LinkedList
+_ **Data Structure path:** src/ffaadd.java
+
+**Team members:**
+1- [فطوم القريشي] , CYN_G2
--- src/ffaadd.java
+++ src/ffaadd.java
@@ -1,5 +1,21 @@
-public class ffaadd { new*
-    public static void main(String[] args) { new*
+import gava.util.linkedList;
+public class ffaadd{
+public static void main(String[] args) { new*
         System.out.println("testv2");
+
+        import java.util.LinkedList;
+public class ffaadd {
+    public static void main (String[] args) {
+        LinkedList<Strinng> menu = new LinkedLdist<>();
+        menu.add("Burger");
+        menu.add("Pizza");
+
+        System.out.println("Project Name: faadd");
+        System.out.println("Data Structure: LinkedList ");
+
+        for (String item : menu) {
+            System.out.println("- Item: "item);
+
+        }
     }
 }
